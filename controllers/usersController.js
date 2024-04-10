8fa6bc5f08fb1ace007b52922ff34658aab679a4
flaviana <flavicastelocc@gmail.com>
2024-04-10 13:22:23 -0300

    adc regex

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 import { db } from "../database/db.js";
 import Crypto from "../utils/crypto.js"
+import Regex from "../utils/regex.js";
 
 export const getUsers = (req, res) => {
     const queryUser = "SELECT * FROM users";
@@ -18,6 +19,13 @@ export const createUser = (req, res) => {
     const password = req.body.password;
     const name =  req.body.name;
     const email =  req.body.email;
+    
+    const regex = new Regex();
+    const isValid = regex.regexPass(password);
+    if(!isValid) return res.status(422).json({error: "Padrão de senha inválido!"});
+    const isValidEmail = regex.regexEmail(email);
+    if(!isValidEmail) return res.status(422).json({error: "Digite um e-mail válido!"});
+    
     const crypto = new Crypto();
     const passwordHash = crypto.cryptPass(password);
 
